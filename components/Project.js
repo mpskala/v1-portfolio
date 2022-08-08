2f5adb18086680b66aeec6ef1667195e97344d30
Matt Skala <mattskala2@gmail.com>
2022-08-08 22:27:59 +0700

    capitalise project titles.

--- components/Project.js
+++ components/Project.js
@@ -6,7 +6,7 @@ const Project = ({ slug, title, description }) => {
       <a className='w-full group'>
         <div className='w-full mb-8'>
           <div className='flex flex-col justify-between md:flex-row'>
-            <h4 className='w-full mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100 group-hover:text-highlight transition-colors duration-300'>
+            <h4 className='w-full mb-2 capitalize text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100 group-hover:text-highlight transition-colors duration-300'>
               {title}
             </h4>
           </div>
