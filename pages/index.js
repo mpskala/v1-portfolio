72a8dff637b7fdaf8e9fe021647f0359f52388fe
Matt Skala <56092174+mpskala@users.noreply.github.com>
2023-03-21 17:21:02 +0000

    Revert "add height to reduce CLS."
    This reverts commit 1eca01b69f40a94d083af219c9c12c4aeb1303f4.

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,7 @@ import Container from '../components/Container'
 export default function Home() {
 	return (
 		<Container>
-			<div
-				className='main flex flex-col justify-center items-start max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pb-16'
-				style={{ height: '16rem' }}
-			>
+			<div className='flex flex-col justify-center items-start max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pb-16'>
 				<div className='flex flex-col-reverse sm:flex-row items-start'>
 					<div className='flex flex-col pr-8'>
 						<h1
