3dea72776e71633764ee6a4874a3d83323db5bc4
Matt Skala <mattskala2@gmail.com>
2023-03-21 18:41:08 +0000

    apply bg styling to nav container.

--- components/Container.js
+++ components/Container.js
@@ -42,8 +42,8 @@ export default function Container(props) {
 				<meta name='twitter:title' content={meta.title} />
 				<meta name='twitter:description' content={meta.description} />
 			</Head>
-			<div className='sticky flex flex-col justify-center px-8'>
-				<nav className='flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto py-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 dark:bg-opacity-75 bg-opacity-75 dark:text-gray-100'>
+			<div className='sticky flex flex-col justify-center px-8 bg-gray-50 dark:bg-gray-900 dark:bg-opacity-75 bg-opacity-75'>
+				<nav className='flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto py-8 sm:pb-16  text-gray-900 dark:text-gray-100'>
 					<a href='#skip' className='skip-nav'>
 						Skip to content
 					</a>
