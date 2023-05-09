d49ef0d6bcd2afd901191c94db15307cd0e658e3
Matt Skala <56092174+mpskala@users.noreply.github.com>
2023-05-09 16:30:09 +0100

    Updates: add external link logo to Spotify tracks.

--- components/spotify/Track.js
+++ components/spotify/Track.js
@@ -11,9 +11,25 @@ const Track = (track) => {
           target='_blank'
           rel='noopener noreferrer'
         >
-          <p className='font-medium text-gray-900 dark:text-gray-100 truncate w-60 sm:w-96 md:w-full md:break-words md:whitespace-normal'>
-            {track.title}
-          </p>
+          <span className='flex items-center font-medium text-gray-900 dark:text-gray-100 '>
+            <p className='truncate max-w-xs sm:max-w-sm md:max-w-[35rem] md:break-words md:whitespace-normal'>
+              {track.title}
+            </p>
+            <svg
+              className='h-4 w-4 ml-1 inline-block align-middle'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'
+              />
+            </svg>
+          </span>
           <p
             className='text-gray-500 mb-4 truncate w-60 sm:w-96 md:w-full md:break-words md:whitespace-normal'
             color='gray.500'
