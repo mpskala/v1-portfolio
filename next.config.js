71be94cbc8ed911e7528b7825ce2b9bba5fcaf0c
Matt Skala <56092174+mpskala@users.noreply.github.com>
2023-10-17 14:02:57 +0100

    test fix.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
-	basePath: '',
-	images: {
-		domains: ['media.graphassets.com', 'images.unsplash.com'],
-	},
+  reactStrictMode: false,
+  basePath: '',
+  images: {
+    domains: ['media.graphassets.com', 'images.unsplash.com'],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
