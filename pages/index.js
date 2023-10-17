6a10122a6ba9d1643a71d6bd9e464113efa70884
Matt Skala <56092174+mpskala@users.noreply.github.com>
2023-10-17 13:42:09 +0100

    add attribute to pdf button.

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,8 @@ export default function Home() {
             </h2>
             <a
               className='px-4 py-2 border-orange-500 transition-all border border-solid text-orange-500 w-fit rounded hover:bg-orange-500 hover:text-white'
-              href='./cv.pdf'
+              download
+              href='/cv.pdf'
             >
               View CV
             </a>
