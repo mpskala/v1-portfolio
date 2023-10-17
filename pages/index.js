25c7e2821153adb79eaef328f7259274792f8264
Matt Skala <56092174+mpskala@users.noreply.github.com>
2023-10-17 12:09:10 +0100

    add CV.

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,12 @@ export default function Home() {
                 FootAsylum
               </a>
             </h2>
+            <a
+              className='px-4 py-2 border-orange-500 transition-all border border-solid text-orange-500 w-fit rounded hover:bg-orange-500 hover:text-white'
+              href='./cv.pdf'
+            >
+              View CV
+            </a>
           </div>
           <div className='w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto'>
             <Image
